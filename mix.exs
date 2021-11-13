dbfb5459eef22a17d78a1b4c5de7ad7a1af738a5
leeduckgo <albertschr@163.com>
2021-11-13 17:39:54 +0800

    feat/update ar_sdk

--- mix.exs
+++ mix.exs
@@ -51,7 +51,7 @@ defmodule FunctionServerBasedOnArweave.MixProject do
       {:plug_cowboy, "~> 2.5"},
 
       # arweave
-      {:arweave_sdk_ex, "~> 0.0.1"},
+      {:arweave_sdk_ex, "~> 0.0.2"},
       {:ecto, "~> 3.7.1", override: true}
     ]
   end
