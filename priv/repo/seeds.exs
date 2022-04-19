5fb6c029c7b3814fe758ac85aec64b1074305efb
leeduckgo <albertschr@163.com>
2022-04-19 23:51:38 +0800

    feat/update seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -19,7 +19,7 @@ tx_ids =
     "kqozWQAcbDr3S_hSEi91O4eQ28tpDvrOKj0xlQ6_dKw", # BlockToAsciiEmojiTranslator
     "rr5p8_FJ4l0KvjhCtIzmxfBNN3UiN2Cv2Ml08ys9odE", # AsciiDrawer
     "UzvTGCxCg0xB2mDqX27vPf_nTz4TjUaGRYZeJ3GsuM0", # Verifier
-    "ypcTmb19L6Xd7mw75tQ8oFDCAYyOoQT9Xb8uKU38jpw", # NftRender.NType
+    "1R7Y8U1bjC0EGePqHDC_0ODY-GiWxOw_rUPQOfXZLGA", # NftRender.NType
   ]
 
 Enum.map(tx_ids, fn tx_id ->
