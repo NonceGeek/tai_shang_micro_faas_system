db8dc6d7ae6656d4cba85916e55ddb9e8490bea2
leeduckgo <albertschr@163.com>
2022-05-08 13:06:06 +0800

    feat/update kv table

---
+++ lib/codes_on_chain/CodesOnChain.Meeting.ex
@@ -0,0 +1,11 @@
+defmodule CodesOnChain.Meeting do
+  @moduledoc """
+    put/get meeting info by signature.
+  """
+
+  alias Components.{KV, Verifier}
+
+  def get_module_doc(), do: @moduledoc
+
+  # TODO
+end
