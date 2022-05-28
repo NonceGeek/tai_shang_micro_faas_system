bcf1425f5dfd6337d3ef789280c82464783c554b
leeduckgo <albertschr@163.com>
2022-05-29 01:02:02 +0800

    update seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -14,11 +14,11 @@ alias FunctionServerBasedOnArweave.OnChainCode
 
 tx_ids_on_arweave =
   [
-    "-6TxJsLSeoXfEhKfGzG5-n65QpAbuiwp4fO_7-2A-vA", # BestBlockHeightGetter
-    "ghBIjdbs2HpGM0Huy3IV0Ynm9OOWxDLkcW6q0X7atqs", # EndpointProvider
-    "kqozWQAcbDr3S_hSEi91O4eQ28tpDvrOKj0xlQ6_dKw", # BlockToAsciiEmojiTranslator
-    "rr5p8_FJ4l0KvjhCtIzmxfBNN3UiN2Cv2Ml08ys9odE", # AsciiDrawer
-    "UzvTGCxCg0xB2mDqX27vPf_nTz4TjUaGRYZeJ3GsuM0", # Verifier
+    # "-6TxJsLSeoXfEhKfGzG5-n65QpAbuiwp4fO_7-2A-vA", # BestBlockHeightGetter
+    # "ghBIjdbs2HpGM0Huy3IV0Ynm9OOWxDLkcW6q0X7atqs", # EndpointProvider
+    # "kqozWQAcbDr3S_hSEi91O4eQ28tpDvrOKj0xlQ6_dKw", # BlockToAsciiEmojiTranslator
+    # "rr5p8_FJ4l0KvjhCtIzmxfBNN3UiN2Cv2Ml08ys9odE", # AsciiDrawer
+    # "UzvTGCxCg0xB2mDqX27vPf_nTz4TjUaGRYZeJ3GsuM0", # Verifier
     "1R7Y8U1bjC0EGePqHDC_0ODY-GiWxOw_rUPQOfXZLGA", # NftRender.NType
   ]
 
@@ -27,7 +27,8 @@ Enum.map(tx_ids_on_arweave, fn tx_id ->
 end)
 
 tx_ids_on_gist =
-  ["8634a21477ea60785783cc0642ba4133"] # Contract Syncer
+  []
+  # ["8634a21477ea60785783cc0642ba4133"] # Contract Syncer
 Enum.map(tx_ids_on_gist, fn tx_id ->
   OnChainCode.create_or_query_by_tx_id(tx_id, "gist")
 end)
