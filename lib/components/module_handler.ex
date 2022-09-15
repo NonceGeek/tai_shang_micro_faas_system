09eac4448e5818100e7120ce81109205060073bd
leeduckgo <albertschr@163.com>
2022-09-15 20:08:01 +0800

    update Components.ModuleHandler

--- lib/components/module_handler.ex
+++ lib/components/module_handler.ex
@@ -1,9 +1,13 @@
 defmodule Components.ModuleHandler do
+  @doc """
+    Example:  Components.ModuleHandler.get_module_name(Components.ModuleHandler.Test)
+  """
   def get_module_name(module_name) do
-    module_name
-    |> get_module_name(:full)
-    |> String.split(".")
-    |> Enum.fetch!(-1)
+    [_, _, module] =
+      module_name
+      |> get_module_name(:full)
+      |> String.split(".", parts: 3)
+    module
   end
   def get_module_name(module_name, :full) do
     Atom.to_string(module_name)
