af31823ba38412bc6b35806c11bca7fc4a4c8a41
leeduckgo <albertschr@163.com>
2023-03-24 20:31:13 +0800

    update with version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FunctionServerBasedOnArweave.MixProject do
   def project do
     [
       app: :function_server_based_on_arweave,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:gettext] ++ Mix.compilers(),
