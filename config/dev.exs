278770de8c4a33ae10ec2fe17ebb6bfa90003dfa
leeduckgo <leeduckgo@gmail.com>
2023-05-26 08:18:14 +0800

    update

--- config/dev.exs
+++ config/dev.exs
@@ -19,7 +19,7 @@ config :tai_shang_micro_faas_system, TaiShangMicroFaasSystem.Repo,
 config :tai_shang_micro_faas_system, TaiShangMicroFaasSystemWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {127, 0, 0, 1}, port: 4003],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
