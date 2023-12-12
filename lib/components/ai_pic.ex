af80b708b2536e6e459796c4920bd7ea03eac458
leeduckgo <leeduckgo@gmail.com>
2023-12-12 16:02:35 +0800

    update ai_pic.ex

--- lib/components/ai_pic.ex
+++ lib/components/ai_pic.ex
@@ -11,16 +11,28 @@ defmodule Components.AiPic do
         Tesla.client([
             # TODO: convert input/output type
             {Tesla.Middleware.BaseUrl, @base},
-            {Tesla.Middleware.Headers, [{"content-type", "text/plain"}, {"x-api-key", Constants.clipdrop_key()}]},
-            {Tesla.Middleware.JSON, engine_opts: [keys: :atoms]}
+            {Tesla.Middleware.Headers, [{"x-api-key", Constants.clipdrop_key()}]},
+            # {Tesla.Middleware.EncodeFormUrlencoded}
         ])
     end
 
-    def do_text_to_image(client, prompt) do
+    def do_text_to_image(_client, prompt) do
         mp =
             Tesla.Multipart.new()
-            |> Tesla.Multipart.add_file_content("prompt=#{prompt}", "sample.txt")
-        Tesla.post(client, "/text-to-image/v1", "prompt=#{prompt}")
+            |> Tesla.Multipart.add_field("prompt", prompt)
+
+        # Tesla.post(client, "/text-to-image/v1", mp)
+        {:ok, %{body: img_raw}} =
+            Tesla.post(
+                "https://clipdrop-api.co/text-to-image/v1", 
+                mp, 
+                headers: [{"x-api-key", Constants.clipdrop_key()}]
+            )
+         # Save file
+        file_name = RandGen.gen_hex(10)
+        File.write("pic_generated/#{file_name}.png", img_raw)
+        file_name
+        # response the link
     end
     # def do_text_to_image(txt) do
     #     IO.puts inspect Constants.clipdrop_key()
