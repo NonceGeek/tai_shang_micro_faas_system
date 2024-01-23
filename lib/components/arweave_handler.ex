049c806b53c94f2de07bef10e445f25d13c19e48
leeduckgo <leeduckgo@gmail.com>
2024-01-24 00:19:16 +0800

    update with arweave components

--- lib/components/arweave_handler.ex
+++ lib/components/arweave_handler.ex
@@ -2,4 +2,17 @@ defmodule Components.ArweaveHandler do
   def get_content(tx_id) do
     ArweaveSdkEx.get_content_in_tx(Constants.get_arweave_node(), tx_id)
   end
+
+  def send_tx(data, tags) do
+    node = Constants.get_arweave_node()
+    # call the priv key local.
+    jwt = ArweaveSdkEx.Wallet.read_jwk_json_from_file("../arweave_wallet.json")
+    reward_coefficient = 1
+    {tx_signed, id, _tx_unsigned} = 
+      ArweaveSdkEx.Wallet.sign_tx(node, data, tags, jwt, reward_coefficient)
+    {:ok, "success submit tx"} = 
+      ArweaveSdkEx.send(node, tx_signed)
+    id  
+  end
+
 end
